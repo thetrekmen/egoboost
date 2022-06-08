67602aacec58645b39b88baafffb0468079cad45
thetrekmen <jonespm1@gmail.com>
2022-06-08 11:43:38 -0400

    inital commit

---
+++ ego-boost.js
@@ -0,0 +1,25 @@
+
+//created a function for username.  the function accepted a string with a default value of "stranger".  if a string was not applied returned invalid
+function promptForUsername(username = "stranger") {
+    if (typeof username === "number") {
+    console.log("enter valid name")
+    }
+    return username
+}
+
+//this is out inventory for our egoBoost program
+const prefix = ["you"];
+const middle = ["look great","will have a great day"];
+const suffix = ["today!"," "];
+
+//created a random number generator for selecting a random string from each of our arrays above.
+let rngPrefix = Math.floor(Math.random() * prefix.length);
+let rngMiddle = Math.floor(Math.random() * middle.length);
+let rngSuffix = Math.floor(Math.random() * suffix.length);
+
+//this function will string interpolate an element from each of our arrays to form our ego boosting message
+ const egoBoost = () => {
+    console.log(`${promptForUsername("Patrick")} ${prefix[rngPrefix]} ${middle[rngMiddle]} ${suffix[rngSuffix]}`)
+}
+
+egoBoost();
